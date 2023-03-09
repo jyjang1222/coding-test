8931d1a9529f3bf83509384f0133daa9fadcf40c
Jun <jyjang1222@naver.com>
2023-03-09 10:30:49 +0900

    [Bronze II] Title: 음계, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+asc = '1 2 3 4 5 6 7 8'
+desc = '8 7 6 5 4 3 2 1'
+
+input = input()
+
+if input == asc:
+    print('ascending')
+elif input == desc:
+    print('descending')
+else:
+    print('mixed')
