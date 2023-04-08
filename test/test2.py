bf231e8160a9dcc59b5d1083cf5011059df8b733
JJY <jyjang1222@naver.com>
2023-04-09 00:26:52 +0900

    update

--- test/test2.py
+++ test/test2.py
@@ -1,10 +1,9 @@
-import sys
-row, col = map(int, sys.stdin.readline().split())
+row, col = map(int, input().split())
 rowcol = [row, col]
 map = ''
 for i in range(row):
-    map += sys.stdin.readline().rstrip()
-# print(map)
+    map += input()
+
 maxSize = min(rowcol) + 1
 maxSize //= 2
 
@@ -54,42 +53,3 @@ if size == 1:
         size = 0
 
 print(size)
-
-# 1 1x1
-# 2 3x3
-# 3 5x5
-# 4 7x7
-
-# size 1
-# 0
-
-# size 2
-# 3*3   4 2
-#  1 
-# 3 5 
-#  7
-# 3*4    5 3
-# 0020
-# 0507
-# 00100
-# 4x4     5 3
-# 0000
-# 0100  5
-# 1010 8 10
-# 0100  13
-
-# size 3  6 4
-#    2 
-#   6 8 
-# 10   14
-#  16 18 
-#    22
-
-# size 4  8 6
-# 0 0 0 3 0 0 0
-# 0 0 9 0 11 0 0
-# 0 15 0 0 0 19 0
-# 21 0 0 0 0 0 27
-# 0 29 0 0 0 33 0
-# 0 0 37 0 39 0 0
-# 0 0 0 45 0 0 0
