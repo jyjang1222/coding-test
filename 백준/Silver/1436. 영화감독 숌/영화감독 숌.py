be06ca21752db6b5d98cb4a0091902fc2b90ed7c
Jun <jyjang1222@naver.com>
2023-04-08 17:45:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 920 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -6,9 +6,8 @@ sample = '666'
 while count < N:
     num += 1
     nan = str(num)
-    for i in range(len(nan) - len(sample) + 1):
-        if nan[i] == '6' and nan[i+1] == '6' and nan[i+2] == '6':
-            count += 1
-            break
+    
+    if sample in nan:
+        count += 1
 
 print(num)
