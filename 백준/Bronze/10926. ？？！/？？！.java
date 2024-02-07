ddff1355bf66451a15b99131235ecb8cf924b299
Jun <jyjang1222@naver.com>
2024-02-07 18:59:54 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12804 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    	Scanner scan = new Scanner(System.in);
+    	String id = scan.nextLine();
+    	
+    	System.out.println(id + "??!");
+ 
+    }
+}
